de00f0f20795448b4030e1932ad52dbd20a8cb7b
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-07-12 15:19:05 -0500

    finished exercise

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace nickelback
 {
@@ -6,7 +8,23 @@ namespace nickelback
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           List<(string Song, string Artist)> goodSongs = new List<(string Song, string Artist)>();
+
+           HashSet<(string, string)> allSongs = new HashSet<(string, string)>();
+
+           allSongs.Add(("Better Days", "Pipe Smokers"));
+           allSongs.Add(("Dog Days", "Set of Pipes"));
+           allSongs.Add(("Something Just Like This", "Chainsmokers & Coldplay"));
+           allSongs.Add(("Alone", "Marshmellow"));
+           allSongs.Add(("Down", "Marian Hill"));
+           allSongs.Add(("Photograph", "Nickelback"));
+           allSongs.Add(("Rockstar", "Nickelback"));
+
+          goodSongs = allSongs.Where(song => song.Item2 != "Nickelback").ToList();
+        
+           foreach ((string Song, string Artist) song in goodSongs) {
+               Console.WriteLine($"{song.Song} by {song.Artist}");
+           }
         }
     }
 }
